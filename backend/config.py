effe9d61e161fec58d48798cfc013f4171eaabf8
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-06-19 22:55:26 +0530

    config

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,25 @@
 import os
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
+
 
 class Config:
     GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
     GROQ_MODEL_NAME = "llama-3.3-70b-versatile"
+
     MONGO_URI = os.environ.get("MONGO_URI")
     DB_NAME = "Articulate.Ai"
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+# -----------------------------
+# MongoDB Connection
+# -----------------------------
+
+
+client = MongoClient(Config.MONGO_URI)
+
+db = client[Config.DB_NAME]
