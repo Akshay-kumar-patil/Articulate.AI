ee02719a98928a2fe78c0c870e158a248e03cc9d
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-06-21 00:43:23 +0530

    kaammm bahut sara baaki hai

---
+++ backend/routes/interview.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+from fastapi import APIRouter,UploadFile, File
+from pydantic import BaseModel
+from backend.services.question_gen import generate_questions
+from backend.services.speech import transcribe_audio
+
+router=APIRouter()
+
+class QuestionRequest(BaseModel):
+    resume_info: dict
+    difficulty: str
+    total_questions: int
+
+@router.post("/generate-questions")
+def get_questions(req: QuestionRequest):
+    questions = generate_questions(req.resume_info, req.difficulty, req.total_questions)
+    return {"questions": questions}
+
+@router.post("/answer-audio")
+def answer_audio(file:UploadFile=File(...)):
+    temp_path=f"temp_path{file.filename}"
+
+    with open(temp_path,"wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    data=transcribe_audio(temp_path)
+
+    os.remove(temp_path)
+
+    return {
+        "message": "Audio transcribed successfully",
+        "text_length": len(data),
+        "preview": data[:300],
+        "text": data
+    }
