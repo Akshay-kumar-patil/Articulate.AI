29e92eed1faa1d3902891391fe45c00a00a42a90
Akshay Kumar Patil <akshaykumarpatil58@gmail.com>
2026-06-24 11:45:43 +0530

    voice->text and text->voice

--- backend/services/speech.py
+++ backend/services/speech.py
@@ -1,5 +1,7 @@
 from groq import Groq
 from backend.config import Config
+import pyttsx3
+
 
 client = Groq(api_key=Config.GROQ_API_KEY)
 
@@ -10,3 +12,11 @@ def transcribe_audio(file_path: str) -> str:
             model="whisper-large-v3"
         )
     return transcription.text                                                                           
+
+def speak_question(text:str):
+    engine=pyttsx3.init()
+    engine.setProperty("rate",150) #speeking speed
+    engine.setProperty("volume", 1.0) # volume 0-1
+    engine.say(text)
+    engine.runAndWait()
+
